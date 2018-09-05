c0922db0025ef0e84e88184f4d4a84572ecfc783
shuff1e <sfxu@foxmail.com>
2018-09-05 20:28:50 +0800

    Create operationAndTimeHeap.go

---
+++ operationAndTimeHeap.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"container/heap"
+)
+
+type opeHeap []reqAndTime
+
+func (h opeHeap) Len() int           { return len(h) }
+func (h opeHeap) Less(i, j int) bool { return h[i].spentTime < h[j].spentTime }
+func (h opeHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h opeHeap) Push(x interface{}) {
+}
+
+func (h opeHeap) Pop() interface{} {
+	return nil
+}
+
+func Push(h opeHeap, value reqAndTime) {
+	if value.spentTime > h[0].spentTime {
+		h[0] = value
+	}
+	heap.Fix(h, 0)
+}
