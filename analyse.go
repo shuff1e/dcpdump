615a3e0a2e05896a0839b378288d6e1173a96b72
shuffle <sfxu@foxmail.com>
2018-09-12 15:32:58 +0800

    Update analyse.go

--- analyse.go
+++ analyse.go
@@ -1,26 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"github.com/google/gopacket/layers"
 	"github.com/couchbase/gomemcached"
 	"github.com/rcrowley/go-metrics"
 	"net"
 	"sort"
 	"time"
+    "strconv"
 )
 
 type MCReqAndTime struct {
 	request gomemcached.MCRequest
 	reqTime time.Time
 	srcIP   net.IP
+    srcPort layers.TCPPort
 	dstIP   net.IP
+    dstPort layers.TCPPort
 }
 
 type MCRespAndTime struct {
 	response gomemcached.MCResponse
 	respTime time.Time
 	srcIP    net.IP
+    srcPort layers.TCPPort
 	dstIP    net.IP
+    dstPort layers.TCPPort
+}
+
+func (req MCReqAndTime) Key() string {
+	return req.srcIP.String()+ req.srcPort.String() + req.dstIP.String() + req.dstPort.String() + strconv.Itoa(int(req.request.Opaque))
+}
+
+func (req MCReqAndTime) modeServer(mode string) string {
+    if mode == "client" {
+        return req.dstIP.String()
+    } else {
+        return req.srcIP.String()
+    }
+}
+
+func (resp MCRespAndTime) Key() string {
+	return resp.dstIP.String() + resp.dstPort.String() + resp.srcIP.String()+ resp.srcPort.String() +  strconv.Itoa(int(resp.response.Opaque))
 }
 
 type reqAndTime struct {
@@ -39,6 +60,15 @@ type counterAndHisto struct {
 	histo   metrics.Histogram
 }
 
+func initMetrics() counterAndHisto {
+    c1 := metrics.NewCounter()
+    c2 := metrics.NewCounter()
+    s := metrics.NewExpDecaySample(1024, 0.015)
+    /* s := metrics.NewUniformSample(1028) */
+    h := metrics.NewHistogram(s)
+    return counterAndHisto{c1,c2,h}
+}
+
 func analyse() {
 	ticker := time.NewTicker(time.Duration(options.printInterval) * time.Second)
 	serverMetrics := make(map[string]counterAndHisto)
@@ -47,57 +77,50 @@ func analyse() {
 	for {
 		select {
 		case req := <-reqChan:
-			if _, ok := rawData[req.srcIP.String()+req.dstIP.String()+fmt.Sprintf("%d", req.request.Opaque)]; !ok {
-				rawData[req.srcIP.String()+req.dstIP.String()+fmt.Sprintf("%d", req.request.Opaque)] = req
+			if _, ok := rawData[req.Key()]; !ok {
+				rawData[req.Key()] = req
 			}
 		case resp := <-respChan:
-			if req, ok := rawData[resp.dstIP.String()+resp.srcIP.String()+fmt.Sprintf("%d", resp.response.Opaque)]; ok {
+			if req, ok := rawData[resp.Key()]; ok {
 				if options.printAll {
-					fmt.Printf("%s, key %s, sent from %15s to %15s at %s, received at %s\n", req.request.Opcode, string(req.request.Key), req.srcIP, req.dstIP, req.reqTime, resp.respTime)
+					Printf("%s, key %s, sent from %21s to %21s at %s, received at %s\n", req.request.Opcode, string(req.request.Key), req.srcIP.String() + ":" +req.srcPort.String(), req.dstIP.String() + ":" + req.dstPort.String(), req.reqTime, resp.respTime)
 				}
 				spentTime := resp.respTime.Sub(req.reqTime)
-				if ch, ok := serverMetrics[req.dstIP.String()]; ok {
-					ch.all.Inc(1)
-					ch.histo.Update(spentTime.Nanoseconds() / 1000)
-				} else {
-					c1 := metrics.NewCounter()
-					c2 := metrics.NewCounter()
-					c1.Inc(1)
-					s := metrics.NewExpDecaySample(1024, 0.015)
-					/* s := metrics.NewUniformSample(1028) */
-					h := metrics.NewHistogram(s)
-					h.Update(spentTime.Nanoseconds() / 1000)
-					serverMetrics[req.dstIP.String()] = counterAndHisto{c1, c2, h}
-				}
+				if _, ok := serverMetrics[req.modeServer(options.mode)]; !ok {
+					serverMetrics[req.modeServer(options.mode)] = initMetrics()
+                }
+                ch := serverMetrics[req.modeServer(options.mode)]
+                ch.all.Inc(1)
+                ch.histo.Update(spentTime.Nanoseconds() / 1000)
 				if spentTime > time.Duration(time.Duration(options.timeout)*time.Millisecond) {
-					serverMetrics[req.dstIP.String()].timeout.Inc(1)
+					ch.timeout.Inc(1)
 				}
-				delete(rawData, req.srcIP.String()+req.dstIP.String()+fmt.Sprintf("%d", req.request.Opaque))
+				delete(rawData, req.Key())
 				Push(data, reqAndTime{req, spentTime})
 			}
 		case <-ticker.C:
-			fmt.Printf("\n\n------------------top %v request with the longest response time-----------------------------------\n", options.topN)
+			Printf("\n\n------------------top %v request with the longest response time-----------------------------------\n", options.topN)
 			sort.Sort(data)
 			for _, x := range data {
 				if x.reqTime != initialTime {
-					fmt.Printf("%s, key %s, sent from %15s to %15s at %s, spent %s\n", x.request.Opcode, string(x.request.Key), x.srcIP, x.dstIP, x.reqTime, x.spentTime)
+					Printf("%s, key %s, sent from %21s to %21s at %s, spent %s\n", x.request.Opcode, string(x.request.Key), x.srcIP.String() + ":" +x.srcPort.String(), x.dstIP.String() + ":" + x.dstPort.String(), x.reqTime, x.spentTime)
 					/* data[i] = reqAndTime{} */
 				}
 			}
 			Heapify(data)
-			fmt.Printf("\n")
+			Printf("\n")
 			for i, x := range serverMetrics {
-				fmt.Printf("metrics of server %s\n", i)
-				fmt.Printf("%v timeout in %v\n", x.timeout.Count(), x.all.Count())
+				Printf("metrics of server %s\n", i)
+				Printf("%v timeout in %v\n", x.timeout.Count(), x.all.Count())
 				ps := x.histo.Percentiles([]float64{0.5, 0.75, 0.95, 0.99})
-				fmt.Printf("min = %.4f ms\n", float64(x.histo.Min())/1000)
-				fmt.Printf("max = %.4f ms\n", float64(x.histo.Max())/1000)
-				fmt.Printf("mean = %.4f ms\n", x.histo.Mean()/1000)
-				fmt.Printf("%%50 <= %.4f ms\n", ps[0]/1000)
-				fmt.Printf("%%75 <= %.4f ms\n", ps[1]/1000)
-				fmt.Printf("%%95 <= %.4f ms\n", ps[2]/1000)
-				fmt.Printf("%%99 <= %v ms\n", ps[3]/1000)
-				fmt.Println()
+				Printf("min = %.4f ms\n", float64(x.histo.Min())/1000)
+				Printf("max = %.4f ms\n", float64(x.histo.Max())/1000)
+				Printf("mean = %.4f ms\n", x.histo.Mean()/1000)
+				Printf("%%50 <= %.4f ms\n", ps[0]/1000)
+				Printf("%%75 <= %.4f ms\n", ps[1]/1000)
+				Printf("%%95 <= %.4f ms\n", ps[2]/1000)
+				Printf("%%99 <= %v ms\n", ps[3]/1000)
+				Println()
 				/* h.Clear() */
 			}
 		case <-rawTicker.C:
