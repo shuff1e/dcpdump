75659a6619949c85b880b4376f1546ffac36c50b
shuffle <sfxu@foxmail.com>
2018-09-12 15:33:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"os"
 	"time"
+    "net/http"
 )
 
 var options struct {
@@ -21,6 +22,7 @@ var options struct {
 	topN          int
 	printAll      bool
 	timeout       int
+    mode          string
 }
 
 func argParse() {
@@ -38,6 +40,8 @@ func argParse() {
 		"whether to print all the info")
 	flag.IntVar(&options.timeout, "timeout", 10,
 		"timeout setting, in milliseconds")
+    flag.StringVar(&options.mode, "mode", "client",
+        "run at server or client")
 	flag.Usage = usage
 	flag.Parse()
 }
@@ -54,11 +58,29 @@ var (
 	handle      *pcap.Handle
 	reqChan     = make(chan MCReqAndTime)
 	respChan    = make(chan MCRespAndTime)
+    httpChan    = make(chan string, 10)
+    writer      http.ResponseWriter
 )
 
 func init() {
 	argParse()
 	data = make(opeHeap, options.topN)
+}
+
+func Println(a ...interface{}) {
+    fmt.Println(a...)
+        /* select { */
+        /* case httpChan <- fmt.Sprintln(a...): */
+        /* default: */
+        /* } */
+}
+
+func Printf(format string, a ...interface{}) {
+    fmt.Printf(format, a...)
+        /* select { */
+        /* case httpChan <- fmt.Sprintf(format, a...): */
+        /* default: */
+        /* } */
 }
 
 func main() {
@@ -82,6 +104,7 @@ func main() {
 	}
 
 	go analyse()
+    /* go httpPrint() */
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
@@ -102,34 +125,44 @@ func dispatch(packet gopacket.Packet) {
 		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
 		// Checksum, SrcIP, DstIP
 
-		// When iterating through packet.Layers() above,
-		// if it lists Payload layer then that is the same as
-		// this applicationLayer. applicationLayer contains the payload
-		applicationLayer := packet.ApplicationLayer()
-		if applicationLayer != nil {
-			payload := applicationLayer.Payload()
-			r := bytes.NewReader(payload)
-			switch payload[0] {
-			case 128:
-				rv := gomemcached.MCRequest{}
-				_, err := rv.Receive(r, nil)
-				if err != nil {
-					fmt.Println("Error decoding some part of the packet:", err)
-				} else {
-					reqChan <- MCReqAndTime{rv, packet.Metadata().CaptureInfo.Timestamp, ip.SrcIP, ip.DstIP}
-				}
-			case 129:
-				rv := gomemcached.MCResponse{}
-				_, err := rv.Receive(r, nil)
-				if err != nil {
-					fmt.Println("Error decoding some part of the packet:", err)
-				} else {
-					respChan <- MCRespAndTime{rv, packet.Metadata().CaptureInfo.Timestamp, ip.SrcIP, ip.DstIP}
-				}
-			default:
-				/* fmt.Printf("%s\n", payload) */
-			}
-		}
+        // Let's see if the packet is TCP
+        tcpLayer := packet.Layer(layers.LayerTypeTCP)
+        if tcpLayer != nil {
+            tcp, _ := tcpLayer.(*layers.TCP)
+
+            // TCP layer variables:
+            // SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+            // Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+
+            // When iterating through packet.Layers() above,
+            // if it lists Payload layer then that is the same as
+            // this applicationLayer. applicationLayer contains the payload
+            applicationLayer := packet.ApplicationLayer()
+            if applicationLayer != nil {
+                payload := applicationLayer.Payload()
+                r := bytes.NewReader(payload)
+                switch payload[0] {
+                case 128:
+                    rv := gomemcached.MCRequest{}
+                    _, err := rv.Receive(r, nil)
+                    if err != nil {
+                        Println("Error decoding some part of the packet:", err)
+                    } else {
+                        reqChan <- MCReqAndTime{rv, packet.Metadata().CaptureInfo.Timestamp, ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort}
+                    }
+                case 129:
+                    rv := gomemcached.MCResponse{}
+                    _, err := rv.Receive(r, nil)
+                    if err != nil {
+                        Println("Error decoding some part of the packet:", err)
+                    } else {
+                        respChan <- MCRespAndTime{rv, packet.Metadata().CaptureInfo.Timestamp, ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort}
+                    }
+                default:
+                    /* fmt.Printf("%s\n", payload) */
+                }
+            }
+        }
 	}
 
 }
