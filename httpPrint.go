aebb303298aa03fb0e7ca5ba3caf2f80e97b2c22
shuffle <sfxu@foxmail.com>
2018-10-15 22:31:19 +0800

    Update httpPrint.go

--- httpPrint.go
+++ httpPrint.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "net/http"
-    "io"
-       )
+	"io"
+	"net/http"
+)
 
 func dcpHandler(w http.ResponseWriter, req *http.Request) {
-    for {
-        select {
-        case info := <-httpChan:
-            io.WriteString(w, info)
-        }
-    }
+	for {
+		select {
+		case info := <-httpChan:
+			io.WriteString(w, info)
+		}
+	}
 }
 
 func httpPrint() {
-    http.HandleFunc("/", dcpHandler)
-    http.ListenAndServe(":12345", nil)
+	http.HandleFunc("/", dcpHandler)
+	http.ListenAndServe(":12345", nil)
 
 }
